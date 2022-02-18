bf846e750245c8dc69ed16f5a6ca93ddd37f94a6
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-18 17:42:27 +0800

    create user

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <CreateComponent></CreateComponent>
+</div>
+</template>
+
+<script>
+import SinglePostComponent from './SinglePostComponent.vue'
+import CreateComponent from './CreateComponent.vue'
+export default {
+    name:'PostComponent',
+    components:{
+        SinglePostComponent,
+        CreateComponent
+    },
+
+    mounted(){
+        this.getPosts()
+    },
+
+    methods:{
+        getPosts(){
+            axios.get('/posts')
+            .then(function(data){
+
+            })
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
