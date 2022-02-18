1f3fc09e642d3e6cfe9449f4cede98d04d494523
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-18 18:22:01 +0800

    update

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -7,15 +7,27 @@
                 <th scope="col">Name</th>
                 <th scope="col">Age</th>
                 <th scope="col">Job</th>
+                <th scope="col">Edit</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="person in people">
-                <th scope="row">{{person.id}}</th>
-                <td> {{person.name}}</td>
-                <td> {{person.age}}</td>
-                <td> {{person.job}}</td>
-            </tr>
+            <template v-for="person in people">
+                <tr :class="isEdit(person.id)? 'd-none': ''">
+                    <th scope="row">{{person.id}}</th>
+                    <td> {{person.name}}</td>
+                    <td> {{person.age}}</td>
+                    <td> {{person.job}}</td>
+                    <td> <a href="" @click.prevent="changeEditPersonId(person.id, person.name, person.age, person.job)" class="btn btn-success">Edit</a></td>
+                </tr>
+                <tr :class="isEdit(person.id)? '': 'd-none'">
+                    <th scope="row">{{person.id}}</th>
+                    <th ><input type="text" v-model="name" class="form-control"></th>
+                    <th ><input type="number" v-model="age" class="form-control"></th>
+                    <th ><input type="text" v-model="job" class="form-control"></th>
+                    <td> <a href=""  @click.prevent="updatePerson(person.id)" class="btn btn-success">Update</a></td>
+                </tr>
+
+            </template>
         </tbody>
     </table>
 </div>
@@ -28,7 +40,11 @@ export default {
 
     data(){
         return {
-            people: null
+            people: null,
+            editPersonId: null,
+            name: null,
+            age: null,
+            job: null,
         }
     },
 
@@ -42,6 +58,29 @@ export default {
             .then(res=>{
                 this.people = res.data
             })
+        },
+
+        updatePerson(id){
+            this.editPersonId = null
+            axios.patch(`/api/people/${id}`,{
+                name: this.name,
+                age: this.age,
+                job: this.job,
+            })
+            .then(res=>{
+                this.getPeople()
+            })
+        },
+
+        changeEditPersonId(id, name, age, job){
+            this.editPersonId = id
+            this.name = name
+            this.age = age
+            this.job = job
+        },
+
+        isEdit(id){
+            return this.editPersonId === id
         }
     }
 
