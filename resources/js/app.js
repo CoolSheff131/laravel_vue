5a59dee43012749fa915673b05a661590c04c5f8
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-20 17:49:10 +0800

    vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import store from "./store";
 import router from "./router";
 import Index from "./components/Index.vue";
 
@@ -11,4 +12,5 @@ const app = new Vue({
         Index,
     },
     router,
+    store,
 });
