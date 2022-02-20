69ad37d1f76026bff330be3410d712a2f82306d1
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-20 11:16:47 +0800

    список персон

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -1,12 +1,44 @@
 <template>
   <div>
-      Index
+      <table class="table">
+          <thead>
+              <tr>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Job</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="person in people">
+                  <th>{{ person.name}}</th>
+                  <th>{{ person.age}}</th>
+                  <th>{{ person.job}}</th>
+              </tr>
+          </tbody>
+      </table>
   </div>
 </template>
 
 <script>
 export default {
+name: 'Index',
+data(){
+    return{
+        people: null
+    }
+},
 
+mounted(){
+this.getPeople()
+},
+methods:{
+    getPeople(){
+        axios.get('/api/people')
+        .then(res=>{
+            this.people = res.data
+        })
+    }
+}
 }
 </script>
 
